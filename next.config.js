343659db3c10995732d06aee15128cbbb9682bea
Dmytro Furman <dmytro.furman@beetroot.se>
2023-06-06 01:31:05 +0300

    UPDATE | next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    DOMAIN_URL: "vercel url",
+    DOMAIN_API: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : process.env.DOMAIN_URL,
+    NEXTAUTH_URL: process.env.DOMAIN_API,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
+}
 
 module.exports = nextConfig
