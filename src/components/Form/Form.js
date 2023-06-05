8a2ef7e23e41eeccafdc0d65d4f04bb73d65a90e
Dmytro Furman <dmytro.furman@beetroot.se>
2023-06-06 00:13:36 +0300

    FIX | form tabs, nextjs 13 error

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,8 @@
+"use client"
+
 import { useState } from 'react'
 import styles from '@/styles/components/Form.module.scss'
 
-import Tab from './Tab'
 import Login from './Login'
 import Register from './Register'
 
@@ -14,8 +15,21 @@ export default function Form() {
 
         <div className={styles.tabs}>
 
-          <Tab active={currentForm === 'login'} handleClick={setCurrentForm('login')}>Login</Tab>
-          <Tab active={currentForm === 'register'} handleClick={setCurrentForm('register')}>Register</Tab>
+          <button
+            className={styles.tab}
+            data-active={currentForm === 'login'}
+            onClick={() => setCurrentForm('login')}
+          >
+            Login
+
+          </button>
+          <button
+            className={styles.tab}
+            data-active={currentForm === 'register'}
+            onClick={() => setCurrentForm('register')}
+          >
+            Register
+          </button>
 
         </div>
 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,8 @@
-import Tab from "./Tab"
 import Form from "./Form"
 import Login from "./Login"
 import Register from "./Register"
 import ErrorMsg from "./ErrorMsg"
 
-Form.Tab = Tab
 Form.Login = Login
 Form.Register = Register
 Form.ErrorMsg = ErrorMsg
